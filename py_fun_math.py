f833262994d936c56862d91cf20eb6e33e871218
Viji Venkadesh <vijivenkadesh@gmail.com>
2023-09-16 16:23:44 +0530

    Python Function 1

--- py_fun_math.py
+++ py_fun_math.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[32]:
+
+
+def sum_range(num=0):
+    '''This function sum all the numbers in the given range'''
+    num1 = int(input('Please enter the number range:'))
+    return sum(range(num1)) + num1
+
+
+# In[34]:
+
+
+sum_range()
+
+
 # In[ ]:
 
 
-def 
+
+
+
+# In[ ]:
+
+
+
 
