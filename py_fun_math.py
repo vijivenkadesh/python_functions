de157dbb5f2d8db5aa40e46797eac645b5a95848
Viji Venkadesh <vijivenkadesh@gmail.com>
2023-09-16 19:42:07 +0530

    Python Function3

--- py_fun_math.py
+++ py_fun_math.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[54]:
+# In[1]:
 
 
 def sum_range1(num1=0):
     '''This function sum all the numbers in the given range'''
     return sum(range(num1)) + num1
-
-
-# In[57]:
-
-
-sum_range1(2)
 
 
 # In[58]:
@@ -25,13 +19,6 @@ def sum_range2(num1=0):
         sum1+=i
         sum2 = sum1 + num1
     return sum2
-
-
-# In[60]:
-
-
-print(sum_range1(1))
-print(sum_range2(1))
 
 
 # In[ ]:
